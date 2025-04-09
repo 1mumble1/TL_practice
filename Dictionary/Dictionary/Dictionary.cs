4ba5f121378844a9af87ab4535a62a1a0cfa8cbf
1mumble1 <mrt47@yandex.ru>
2025-04-09 16:08:09 +0300

    Complete Dictionary homework

---
+++ Dictionary/Dictionary/Dictionary.cs
@@ -0,0 +1,81 @@
+﻿namespace Dictionary;
+
+public class Dictionary
+{
+    private const char Delimeter = ':';
+    private Dictionary<string, string> _dictionary = new();
+
+    public void Initialize(string path)
+    {
+        using (StreamReader reader = new StreamReader(path))
+        {
+            string? line;
+            while ((line = reader.ReadLine()) is not null)
+            {
+                string[] splittedLine = line.Split(Delimeter);
+                if (splittedLine.Length > 2)
+                {
+                    Console.WriteLine($"Failed to recognize this line: {line}");
+                    continue;
+                }
+
+                string word = splittedLine[0];
+                string translations = splittedLine[1];
+
+                if (_dictionary.ContainsKey(word))
+                {
+                    Console.WriteLine($"Failed to add: {line}, because '{word}' already added");
+                    continue;
+                }
+                _dictionary.Add(word, translations);
+            }
+
+            Console.WriteLine("Press any button to continue work with dictionary...");
+            Console.ReadKey();
+        }
+    }
+
+    public bool IsEmpty()
+    {
+        return _dictionary.Count == 0;
+    }
+
+    public bool FindWord(string word, out string translate)
+    {
+        if (_dictionary.ContainsKey(word))
+        {
+            translate = _dictionary[word];
+            return true;
+        }
+
+        if (_dictionary.ContainsValue(word))
+        {
+            translate = _dictionary.FirstOrDefault(w => w.Value == word).Key;
+            return true;
+        }
+
+        translate = word;
+        return false;
+    }
+
+    public bool AddNewWord(string word, string translation)
+    {
+        if (!_dictionary.ContainsKey(word))
+        {
+            _dictionary.Add(word, translation);
+            return true;
+        }
+        return false;
+    }
+
+    public void SaveToFile(string path)
+    {
+        using (StreamWriter sw = new StreamWriter(path))
+        {
+            foreach (var pair in _dictionary)
+            {
+                sw.WriteLine($"{pair.Key}{Delimeter}{pair.Value}");
+            }
+        }
+    }
+}
---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,157 @@
+﻿
+
+using System.Collections.Generic;
+using System.Transactions;
+
+namespace Dictionary;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        string filePath = string.Empty;
+        Dictionary dictionary = new();
+
+        Console.WriteLine("This console application was created as dictionary.");
+        TryInitializeDictionary(dictionary, ref filePath);
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Menu: \n" +
+                "1 - Translate word\n" +
+                "2 - Add new translation\n" +
+                "0 - Exit");
+            ConsoleKeyInfo userInput = Console.ReadKey();
+            Console.WriteLine();
+            switch (userInput.Key)
+            {
+                case ConsoleKey.D1:
+                    {
+                        GetTranslation(dictionary);
+                        break;
+                    }
+                case ConsoleKey.D2:
+                    {
+                        AddNewTranslation(dictionary);
+                        break;
+                    }
+                case ConsoleKey.D0:
+                    {
+                        SaveDictionary(dictionary, ref filePath);
+                        return;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Invalid input");
+                        Console.ReadKey();
+                        break;
+                    }
+            }
+        }
+    }
+
+    private static void TryInitializeDictionary(Dictionary dictionary, ref string path)
+    {
+        Console.WriteLine("If you want initialize dictionary, press Enter, else click any button: ");
+
+        ConsoleKeyInfo userInputConfirmation = Console.ReadKey();
+        if (userInputConfirmation.Key != ConsoleKey.Enter)
+        {
+            return;
+        }
+
+        Console.Write("Enter file path: ");
+        path = GetString();
+        dictionary.Initialize(path);
+    }
+
+    private static void SaveDictionary(Dictionary dictionary, ref string path)
+    {
+        Console.WriteLine("Application is closing, wait...");
+        if (string.IsNullOrEmpty(path))
+        {
+            Console.Write("Enter file path: ");
+            path = GetString();
+        }
+
+        dictionary.SaveToFile(path);
+
+        Console.WriteLine("Changes saved successfully! Goodbye");
+    }
+
+    private static void AddNewTranslation(Dictionary dictionary)
+    {
+        Console.Write("Type word you want to add: ");
+        string word = GetString();
+
+        Console.Write($"Type translation of '{word}': ");
+        string translation = GetString();
+
+        AddNewPair(word, translation, dictionary);
+    }
+
+    private static void AddNewPair(string word, string translation, Dictionary dictionary)
+    {
+        if (dictionary.AddNewWord(word, translation))
+        {
+            Console.WriteLine($"Pair '{word}' - '{translation}' successfully added!");
+        }
+        else
+        {
+            Console.WriteLine($"Failed to add pair  '{word}' - '{translation}'");
+        }
+        Console.ReadKey();
+    }
+
+    private static void GetTranslation(Dictionary dictionary)
+    {
+        if (dictionary.IsEmpty())
+        {
+            Console.WriteLine("Dictionary is empty! Add words to get translations");
+            Console.ReadKey();
+            return;
+        }
+
+        Console.Write("Enter word for translating: ");
+        string word = GetString();
+        if (!dictionary.FindWord(word, out var translate))
+        {
+            TryAddNewWord(word, dictionary);
+        }
+        else
+        {
+            Console.WriteLine($"Translation: {translate}");
+            Console.ReadKey();
+        }
+    }
+
+    private static void TryAddNewWord(string word, Dictionary dictionary)
+    {
+        Console.WriteLine($"Word '{word}' not found in dictionary. Do you want to add this word?");
+        Console.WriteLine("Press Enter to confirm, any other button to discard");
+        ConsoleKeyInfo userInputConfirmation = Console.ReadKey();
+        Console.WriteLine();
+        if (userInputConfirmation.Key != ConsoleKey.Enter)
+        {
+            return;
+        }
+
+        Console.Write($"Type translation of '{word}': ");
+        string translation = GetString();
+
+        AddNewPair(word, translation, dictionary);
+    }
+
+    private static string GetString()
+    {
+        string? str = Console.ReadLine();
+        while (string.IsNullOrWhiteSpace(str))
+        {
+            Console.WriteLine("This parameter cannot be empty. Try again");
+            str = Console.ReadLine();
+        }
+
+        return str;
+    }
+}
